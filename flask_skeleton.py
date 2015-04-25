18098952bada8e8d237c05d036f1e18df67bf68b
Michael Herman <hermanmu@gmail.com>
2015-04-25 14:19:34 -0600

    third task

--- flask_skeleton.py
+++ flask_skeleton.py
@@ -6,6 +6,12 @@ import argparse
 import shutil
 import jinja2
 import codecs
+import subprocess
+
+if sys.version_info < (3, 0):
+    from shutilwhich import which
+else:
+    from shutil import which
 
 
 # Globals #
@@ -26,6 +32,7 @@ def main(argv):
     parser = argparse.ArgumentParser(description='Scaffold a Flask Skeleton.')
     parser.add_argument('appname', help='The application name')
     parser.add_argument('-s', '--skeleton', help='The skeleton folder to use.')
+    parser.add_argument('-b', '--bower', help='Install dependencies via bower')
     args = parser.parse_args()
 
     # Variables #
@@ -47,6 +54,25 @@ def main(argv):
     }
     with open(os.path.join(fullpath, 'project', 'config.py'), 'w') as fd:
         fd.write(template.render(template_var))
+
+    # Add bower dependencies
+    if args.bower:
+        bower = args.bower.split(',')
+        bower_exe = which('bower')
+        if bower_exe:
+            os.chdir(os.path.join(fullpath, 'project', 'static'))
+            for dependency in bower:
+                output, error = subprocess.Popen(
+                    [bower_exe, 'install', dependency],
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE
+                ).communicate()
+                # print(output)
+                if error:
+                    print("An error occurred with Bower")
+                    print(error)
+        else:
+            print("Could not find bower. Ignoring.")
 
 
 if __name__ == '__main__':
