f47f6d935c46fe9c2f0a6162fcc6bf356d153d38
Michael Herman <hermanmu@gmail.com>
2016-07-31 21:06:46 -0600

    refactor

--- flask_skeleton.py
+++ flask_skeleton.py
@@ -3,17 +3,16 @@
 import sys
 import os
 import argparse
+import shutil
 import jinja2
 import codecs
 import subprocess
-import shutil
+import platform
 
 if sys.version_info < (3, 0):
     from shutilwhich import which
 else:
     from shutil import which
-
-import platform
 
 
 # Globals #
@@ -22,12 +21,14 @@ cwd = os.getcwd()
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
 # Jinja2 environment
-template_loader = jinja2.FileSystemLoader(
-    searchpath=os.path.join(script_dir, "templates"))
+template_loader = jinja2.FileSystemLoader(searchpath=os.path.join(script_dir, "templates"))
 template_env = jinja2.Environment(loader=template_loader)
 
 
-def get_arguments(argv):
+def main(argv):
+
+    # Arguments #
+
     parser = argparse.ArgumentParser(description='Scaffold a Flask Skeleton.')
     parser.add_argument('appname', help='The application name')
     parser.add_argument('-s', '--skeleton', help='The skeleton folder to use.')
@@ -35,26 +36,6 @@ def get_arguments(argv):
     parser.add_argument('-v', '--virtualenv', action='store_true')
     parser.add_argument('-g', '--git', action='store_true')
     args = parser.parse_args()
-    return args
-
-
-def generate_brief(args):
-    template_var = {
-        'pyversion': platform.python_version(),
-        'appname': args.appname,
-        'bower': args.bower,
-        'virtualenv': args.virtualenv,
-        'skeleton': args.skeleton,
-        'path': os.path.join(cwd, args.appname),
-        'git': args.git
-    }
-    template = template_env.get_template('brief.jinja2')
-    return template.render(template_var)
-
-
-def main(args):
-
-    print("\nScaffolding...")
 
     # Variables #
 
@@ -62,14 +43,30 @@ def main(args):
     fullpath = os.path.join(cwd, appname)
     skeleton_dir = args.skeleton
 
+    # Summary #
+
+    def generate_brief(template_var):
+        template = template_env.get_template('brief.jinja2')
+        return template.render(template_var)
+
+    template_var = {
+        'pyversion': platform.python_version(),
+        'appname': appname,
+        'bower': args.bower,
+        'virtualenv': args.virtualenv,
+        'skeleton': args.skeleton,
+        'path': fullpath,
+        'git': args.git
+    }
+
+    print(generate_brief(template_var))
+
     # Tasks #
 
     # Copy files and folders
-    print("Copying files and folders...")
     shutil.copytree(os.path.join(script_dir, skeleton_dir), fullpath)
 
     # Create config.py
-    print("Creating the config...")
     secret_key = codecs.encode(os.urandom(32), 'hex').decode('utf-8')
     template = template_env.get_template('config.jinja2')
     template_var = {
@@ -80,7 +77,6 @@ def main(args):
 
     # Add bower dependencies
     if args.bower:
-        print("Adding bower dependencies...")
         bower = args.bower.split(',')
         bower_exe = which('bower')
         if bower_exe:
@@ -91,6 +87,7 @@ def main(args):
                     stdout=subprocess.PIPE,
                     stderr=subprocess.PIPE
                 ).communicate()
+                # print(output)
                 if error:
                     print("An error occurred with Bower")
                     print(error)
@@ -100,7 +97,6 @@ def main(args):
     # Add a virtualenv
     virtualenv = args.virtualenv
     if virtualenv:
-        print("Adding a virtualenv...")
         virtualenv_exe = which('pyvenv')
         if virtualenv_exe:
             output, error = subprocess.Popen(
@@ -133,7 +129,6 @@ def main(args):
 
     # Git init
     if args.git:
-        print("Initializing Git...")
         output, error = subprocess.Popen(
             ['git', 'init', fullpath],
             stdout=subprocess.PIPE,
@@ -151,21 +146,4 @@ def main(args):
 
 
 if __name__ == '__main__':
-    arguments = get_arguments(sys.argv)
-    print(generate_brief(arguments))
-    if sys.version_info < (3, 0):
-        input = raw_input
-    proceed = input("\nProceed (yes/no)? ")
-    valid = ["yes", "y", "no", "n"]
-    while True:
-        if proceed.lower() in valid:
-            if proceed.lower() == "yes" or proceed.lower() == "y":
-                main(arguments)
-                print("Done!")
-                break
-            else:
-                print("Goodbye!")
-                break
-        else:
-            print("Please respond with 'yes' or 'no' (or 'y' or 'n').")
-            proceed = input("\nProceed (yes/no)? ")
+    main(sys.argv)
